f5279cbcdcdfc564f2a961b9d8ae655e367a07cc
Khuram Nawaz Khayam <kn.khayam@outlook.com>
2022-06-22 22:35:05 +0500

    First Version

---
+++ Happy Egg/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Happy_Egg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.character = new System.Windows.Forms.PictureBox();
+            this.fallTimer = new System.Windows.Forms.Timer(this.components);
+            this.liftTimer = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.startPanel = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.instructionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.highScoreLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.character)).BeginInit();
+            this.startPanel.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // character
+            // 
+            this.character.Image = global::Happy_Egg.Properties.Resources.happy_Egg;
+            this.character.Location = new System.Drawing.Point(161, 47);
+            this.character.Name = "character";
+            this.character.Size = new System.Drawing.Size(41, 38);
+            this.character.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.character.TabIndex = 0;
+            this.character.TabStop = false;
+            // 
+            // fallTimer
+            // 
+            this.fallTimer.Interval = 10;
+            this.fallTimer.Tick += new System.EventHandler(this.fallTimer_Tick);
+            // 
+            // liftTimer
+            // 
+            this.liftTimer.Interval = 10;
+            this.liftTimer.Tick += new System.EventHandler(this.liftTimer_Tick);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(353, 22);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(80, 23);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "Score: 0";
+            // 
+            // startPanel
+            // 
+            this.startPanel.Controls.Add(this.label2);
+            this.startPanel.Controls.Add(this.menuStrip1);
+            this.startPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.startPanel.Location = new System.Drawing.Point(0, 0);
+            this.startPanel.Name = "startPanel";
+            this.startPanel.Size = new System.Drawing.Size(484, 395);
+            this.startPanel.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(484, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.playToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // playToolStripMenuItem
+            // 
+            this.playToolStripMenuItem.Name = "playToolStripMenuItem";
+            this.playToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.playToolStripMenuItem.Text = "Play";
+            this.playToolStripMenuItem.Click += new System.EventHandler(this.playToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.instructionsToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // instructionsToolStripMenuItem
+            // 
+            this.instructionsToolStripMenuItem.Name = "instructionsToolStripMenuItem";
+            this.instructionsToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.instructionsToolStripMenuItem.Text = "Instructions";
+            this.instructionsToolStripMenuItem.Click += new System.EventHandler(this.instructionsToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(186, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "start";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkRed;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(485, 19);
+            this.panel1.TabIndex = 3;
+            // 
+            // highScoreLabel
+            // 
+            this.highScoreLabel.AutoSize = true;
+            this.highScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.highScoreLabel.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highScoreLabel.Location = new System.Drawing.Point(12, 22);
+            this.highScoreLabel.Name = "highScoreLabel";
+            this.highScoreLabel.Size = new System.Drawing.Size(128, 23);
+            this.highScoreLabel.TabIndex = 1;
+            this.highScoreLabel.Text = "High Score: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(484, 395);
+            this.Controls.Add(this.startPanel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.highScoreLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.character);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.character)).EndInit();
+            this.startPanel.ResumeLayout(false);
+            this.startPanel.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox character;
+        private System.Windows.Forms.Timer fallTimer;
+        private System.Windows.Forms.Timer liftTimer;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Panel startPanel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem playToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem instructionsToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label highScoreLabel;
+    }
+}
+
---
+++ Happy Egg/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Happy_Egg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+        SoundPlayer bg = new SoundPlayer();
+        SoundPlayer die = new SoundPlayer();
+
+        int fallSpeed = 2; // character fall speed //
+        int liftUpSpeed = 2; // lift up speed //
+        int moveSpeed = 6; // character left/right move speed //
+        int direction = 0; // direction to use left/right for character use only to move left/right//
+        int liftWidth = 80; // lift widht // default is 80 //
+        int liftHeight = 20; //lift depth // default is 20 //
+        int liftFasla = 120; /// aik lift say dusri lift tak fasla //
+        int score = 0; /// score //
+
+        Panel[] lift = new Panel[10];
+        Random random = new Random();
+        private void init()
+        {
+            bg.SoundLocation = "Resources/background.wav";
+            bg.PlayLooping();
+            die.SoundLocation = "Resources/die.wav";
+            for(int i=0;i<10;i++)
+            {
+                lift[i] = new Panel();
+                lift[i].Width = liftWidth;
+                lift[i].Height = liftHeight;
+                lift[i].Left = random.Next(0, this.Width - liftWidth);
+                lift[i].Top = this.Height / 2 + (i * liftFasla);
+                lift[i].BackColor = Color.Black;
+                lift[i].Tag = "noScore"; // to use scoring purpose only //
+                this.Controls.Add(lift[i]);
+            }
+            highScoreLabel.Text = "High Score: " + Properties.Settings.Default.highScore.ToString();
+        }
+
+        private void checker()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (character.Left + character.Width > lift[i].Left && character.Left < lift[i].Left + lift[i].Width)
+                {
+                    if (character.Top + character.Height > lift[i].Top && character.Top + character.Height < lift[i].Top + lift[i].Height)
+                    {
+                        character.Top = lift[i].Top - character.Height;
+                    }
+                }
+                if (character.Top + character.Height > lift[i].Top && character.Top + character.Height < lift[i].Top + lift[i].Height)
+                {
+                    if (lift[i].Tag == "noScore")
+                    {
+                        score++;
+                        int hs = Properties.Settings.Default.highScore;
+                        if (score > hs)
+                        {
+                            highScoreLabel.Text = "High Score: " + score;
+                            Properties.Settings.Default.highScore = score;
+                            Properties.Settings.Default.Save();
+                        }
+                        if (score % 10 == 0)
+                        {
+                            if (score < 30)
+                            {
+                                liftUpSpeed++;
+                                moveSpeed++;
+                                fallSpeed++;
+                            }
+                        }
+                        scoreLabel.Text = "Score: " + score;
+                        lift[i].Tag = "Score";
+                    }
+                }
+            }
+            if (character.Top > this.Height - character.Height + 2 || character.Top < panel1.Height)
+            {
+                fallTimer.Stop();
+                liftTimer.Stop();
+                die.Play();
+
+                if (MessageBox.Show("Game Over!\nPlay Again?", "Game Over", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    ///////////////
+                    liftUpSpeed = 2;
+                    fallSpeed = 2;
+                    moveSpeed = 6;
+                    direction = 0;
+                    //////////////
+                    bg.PlayLooping();
+                    score = 0;
+                    scoreLabel.Text = "Score: 0";
+                    character.Top = character.Height + 10;
+                    character.Left = Width / 2;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        lift[i].Top = this.Height / 2 + (i * liftFasla);
+                        lift[i].Tag = "noScore";
+                    }
+                    fallTimer.Start();
+                    liftTimer.Start();
+
+                }
+                else
+                    Close();
+            }
+            
+        }
+
+        private void fallTimer_Tick(object sender, EventArgs e)
+        {
+            character.Top += fallSpeed;
+            if (character.Left < -1)
+                character.Left = Width - character.Width;
+            if (character.Left + character.Width > Width + 1)
+                character.Left = 1;
+            if (direction == 1)
+                character.Left += moveSpeed;
+            else if (direction == -1)
+                character.Left -= moveSpeed;
+            checker();
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+                direction = -1;
+            if (e.KeyCode == Keys.Right)
+                direction = 1;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+                direction = 0;
+        }
+
+        private void liftTimer_Tick(object sender, EventArgs e)
+        {
+            for(int i=0;i<10;i++)
+            {
+                lift[i].Top -= liftUpSpeed;
+                if(lift[i].Top < -liftHeight)
+                {
+                    lift[i].Top = (liftFasla + liftHeight) * 9;
+                    lift[i].Left = random.Next(0, this.Width - liftWidth);
+                    lift[i].Tag = "noScore";
+                }
+            }
+        }
+
+     
+        private void playToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            startPanel.Hide();
+            fallTimer.Start();
+            liftTimer.Start();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void instructionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string instructions = "Use Arrow Keys Left/Right to move the Egg.\n" +
+               "Don't let the egg dropped.\n" +
+               "Reach the maximum Score.";
+            MessageBox.Show(instructions);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            playToolStripMenuItem_Click(sender, e);
+        }
+    }
+}
